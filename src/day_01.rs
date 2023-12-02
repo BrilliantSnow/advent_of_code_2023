540f030183da8d8ba3fbce5787046f7ee9c06303
BrilliantSnow <SnowBastion@gmail.com>
2023-12-02 12:27:53 -0600

    1.2 using regex to find first and last digits

--- src/day_01.rs
+++ src/day_01.rs
@@ -1,4 +1,7 @@
+use std::collections::HashMap;
+
 use crate::input_util;
+use regex::Regex;
 
 pub fn part_one(input_dir: &str) -> i64 {
     let first_digit = |x: &char| x.is_digit(10);
@@ -20,6 +23,75 @@ pub fn part_one(input_dir: &str) -> i64 {
                 .find(first_digit)
                 .map(to_digit)
                 .expect("Input line has at least one digit");
+            // product of first and last digits
+            return first as i64 * 10 + last as i64;
+        })
+        .sum()
+}
+
+pub fn find_first_digit(input: &str) -> u32 {
+    let written_digits = [
+        ("one", "1"),
+        ("two", "2"),
+        ("three", "3"),
+        ("four", "4"),
+        ("five", "5"),
+        ("six", "6"),
+        ("seven", "7"),
+        ("eight", "8"),
+        ("nine", "9"),
+    ];
+
+    let written_digit_map = HashMap::from(written_digits);
+
+    let re = Regex::new(r"(one|two|three|four|five|six|seven|eight|nine|\d)").unwrap();
+    if let Some(first) = re.find(input) {
+        // println!("Input:\n{}\n{}", input, first.as_str());
+        if let Some(replaced) = written_digit_map.get(first.as_str()) {
+            return replaced.parse().expect("it is a digit");
+        } else {
+            return first.as_str().parse().expect("it is a digit");
+        }
+    }
+    panic!("No digits found in input string");
+}
+
+pub fn find_last_digit(input: &str) -> u32 {
+    let written_digits = [
+        ("one", "1"),
+        ("two", "2"),
+        ("three", "3"),
+        ("four", "4"),
+        ("five", "5"),
+        ("six", "6"),
+        ("seven", "7"),
+        ("eight", "8"),
+        ("nine", "9"),
+    ];
+
+    let written_digit_map = HashMap::from(written_digits);
+
+    let re = Regex::new(r".*(one|two|three|four|five|six|seven|eight|nine|\d)").unwrap();
+    let (_line, captures) = re.captures(input).unwrap().extract::<1>();
+    let first_capture = captures[0];
+    // println!("Input:\n{}\n{}", input, first_capture);
+    if let Some(first_digit) = written_digit_map.get(first_capture) {
+        // first capture was a written out digit
+        return first_digit.parse().expect("it is a digit");
+    } else {
+        // first capture is a digit
+        return first_capture.parse().expect("it is a digit");
+    }
+}
+
+pub fn part_two(input_dir: &str) -> i64 {
+    input_util::read_file(&input_dir)
+        .map(|line_read| {
+            let line = line_read.expect("The input file is parsable");
+            // get first digit
+            let first = find_first_digit(&line);
+            // get last digit
+            let last = find_last_digit(&line);
             // product of first and last digits
             return first as i64 * 10 + last as i64;
         })
--- src/main.rs
+++ src/main.rs
@@ -5,6 +5,11 @@ fn main() {
     println!("Welcome to my advent of code 2023 adventure!");
     // day 1
     println!("Day One");
-    let day_one_part_one = day_01::part_one("src/res/day_01.txt");
-    println!("\t1: {}", day_one_part_one);
+    let input = "src/res/day_01.txt";
+
+    let output = day_01::part_one(input);
+    println!("\t1: {}", output);
+
+    let output = day_01::part_two(input);
+    println!("\t2: {}", output);
 }
