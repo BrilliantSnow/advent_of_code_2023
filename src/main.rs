c2568177b6d1597c12a83ea43584abcdffe2e743
BrilliantSnow <SnowBastion@gmail.com>
2023-12-03 00:28:32 -0600

    day 3 checkpoint

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,17 @@
-use crate::{benchmark_util::Solution, day_01::Day01, day_02::Day02};
+use crate::{benchmark_util::Solution, day_01::Day01, day_02::Day02, day_03::Day03};
 
 use enum_dispatch::enum_dispatch;
 
 mod benchmark_util;
 mod day_01;
 mod day_02;
+mod day_03;
 mod input_util;
 
 fn main() {
     println!("Welcome to my advent of code 2023 adventure!");
-    let first = 1;
-    let last = 2;
+    let first = 3;
+    let last = 3;
     for problem in first..=last {
         println!("======== Day {} =========", problem);
         run_day(solution(problem), problem);
@@ -21,16 +22,18 @@ fn main() {
 pub enum Day {
     Day01,
     Day02,
+    Day03,
 }
 
 fn solution(day: usize) -> Day {
     match day {
         1 => Day::Day01(Day01 {}),
         2 => Day::Day02(Day02 {}),
+        3 => Day::Day03(Day03 {}),
         _ => panic!("not implemented"),
     }
 }
 
 fn run_day(day: Day, number: usize) {
-    day.test(input_util::get_file_path(number).as_str(), 100);
+    day.test(input_util::get_file_path(number).as_str(), 1);
 }
