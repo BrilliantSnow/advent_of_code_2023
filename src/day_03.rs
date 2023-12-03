f0a2d5fc9a6ad8d428555d1526ee37e838afbc12
BrilliantSnow <SnowBastion@gmail.com>
2023-12-03 01:02:56 -0600

    part 1 finished

--- src/day_03.rs
+++ src/day_03.rs
@@ -14,47 +14,39 @@ struct PartNumber {
 impl PartNumber {
     fn valid_part(&self, above: Option<&String>, beside: &str, below: Option<&String>) -> bool {
         let length = beside.len();
-        let range = if self.start == 0 { 0 } else { self.start - 1 }..(self.end + 1).min(length - 1);
+
+        let range = if self.start == 0 { 0 } else { self.start - 1 }..(self.end + 1).min(length);
         let level: Vec<char> = beside[range.clone()].chars().collect();
-        let mut adjacent_slices = vec![];
-        let mut valid = false;
         if let Some(higher) = above {
             let higher = &higher[range.clone()];
             if higher
                 .chars()
                 .filter(|letter| '.'.ne(letter))
+                .filter(|letter| !letter.is_numeric())
                 .count()
                 > 0
             {
-                valid = true;
-                // return true;
+                return true;
             }
-            adjacent_slices.push(higher);
         }
-        if '.'.ne(level.get(0).unwrap()) || '.'.ne(level.last().unwrap()) {
-            valid = true;
-            // return true;
+        let first = level.get(0).unwrap();
+        let last = level.last().unwrap();
+        let first_is_symbol = '.'.ne(first) && !first.is_numeric();
+        let second_is_symbol = '.'.ne(last) && !last.is_numeric();
+        if first_is_symbol || second_is_symbol {
+            return true;
         }
-        adjacent_slices.push(&beside[range.clone()]);
         if let Some(lower) = below {
             let lower = &lower[range.clone()];
             if lower
                 .chars()
                 .filter(|letter| '.'.ne(letter))
+                .filter(|letter| !letter.is_numeric())
                 .count()
                 > 0
             {
-                valid = true;
-                // return true;
+                return true;
             }
-            adjacent_slices.push(lower);
-        }
-        if valid {
-            println!("");
-            for line in adjacent_slices {
-                println!("{}", line);
-            }
-            return true;
         }
         return false;
     }
@@ -62,23 +54,21 @@ impl PartNumber {
 
 impl Solution for Day03 {
     fn part_one(&self, file_path: &str) -> i64 {
-        let mut part_numbers = vec![];
         let number_pattern = Regex::new(r"(\d+)").unwrap();
         let lines: Vec<String> = input_util::read_file_buffered(file_path)
             .flatten()
             .collect();
-        for (index, line) in lines.iter().enumerate() {
-            for find in number_pattern.find_iter(line) {
-                part_numbers.push(PartNumber {
+        lines
+            .iter()
+            .enumerate()
+            .flat_map(|(index, line)| {
+                number_pattern.find_iter(&line).map(move |find| PartNumber {
                     start: find.start(),
                     end: find.end(),
                     height: index,
                     number: find.as_str().parse().expect("Parsing a bunch of digits"),
-                });
-            }
-        }
-        part_numbers
-            .iter()
+                })
+            })
             .filter(|part| {
                 part.valid_part(
                     lines.get(part.height - 1),
