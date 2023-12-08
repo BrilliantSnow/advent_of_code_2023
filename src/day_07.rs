a5134fada70b83fa4d5bd2ae36f7a2f75aa1d222
BrilliantSnow <SnowBastion@gmail.com>
2023-12-07 23:08:19 -0600

    part 2 day 7

--- src/day_07.rs
+++ src/day_07.rs
@@ -54,13 +54,13 @@ impl Ord for PokerHand {
                 .zip(right.chars())
                 .find(|(left, right)| left != right)
             {
-                let first = CARDS
+                let first = PART2CARDS
                     .iter()
                     .enumerate()
                     .find(|(_, x)| first_diff.0.eq(x))
                     .unwrap()
                     .0;
-                let second = CARDS
+                let second = PART2CARDS
                     .iter()
                     .enumerate()
                     .find(|(_, x)| first_diff.1.eq(x))
@@ -85,6 +85,10 @@ const CARDS: [char; 13] = [
     'A', 'K', 'Q', 'J', 'T', '9', '8', '7', '6', '5', '4', '3', '2',
 ];
 
+const PART2CARDS: [char; 13] = [
+    'A', 'K', 'Q', 'T', '9', '8', '7', '6', '5', '4', '3', '2', 'J',
+];
+
 impl From<String> for PokerHand {
     fn from(value: String) -> Self {
         let mut letter_count: Vec<(char, i64)> = value
@@ -103,19 +107,37 @@ impl From<String> for PokerHand {
         println!("Letter occurances: {:?}", letter_count);
         letter_count.sort_by_key(|pair| pair.1);
         letter_count.reverse();
+
+        let total_jokers = letter_count.iter().find(|x| x.0 == 'J').map(|x| x.1);
+        let most_common_non_joker = letter_count.iter_mut().find(|x| x.0 != 'J');
+        if let Some(total) = total_jokers {
+            if let Some(add_to) = most_common_non_joker {
+                add_to.1 += total;
+                letter_count.remove(
+                    letter_count
+                        .iter()
+                        .enumerate()
+                        .find(|x| x.1 .0 == 'J')
+                        .unwrap()
+                        .0,
+                );
+            }
+        }
+
         println!("Sorted letters: {:?}", letter_count);
         if letter_count.len() == 1 {
             return PokerHand::FiveOfAKind(value);
         }
         let most_popular = (letter_count.get(0).unwrap(), letter_count.get(1).unwrap());
-        match (most_popular.0, most_popular.1) {
+        match most_popular {
+            ((_, 5), _) => PokerHand::FiveOfAKind(value),
             ((_, 4), _) => PokerHand::FourOfAKind(value),
             ((_, 3), (_, 2)) => PokerHand::FullHouse(value),
             ((_, 3), _) => PokerHand::ThreeOfAKind(value),
             ((_, 2), (_, 2)) => PokerHand::TwoPair(value),
             ((_, 2), _) => PokerHand::OnePair(value),
             ((_, 1), _) => PokerHand::HighCard(value),
-            _ => panic!("Hand has more than 5 cards??"),
+            _ => unreachable!("Hand has more than 5 cards??"),
         }
     }
 }
@@ -140,6 +162,20 @@ impl Solution for Day07 {
     }
 
     fn part_two(&self, file_path: &str) -> i64 {
-        todo!()
+        let mut poker_hands: Vec<(PokerHand, i64)> = input_util::read_file_buffered(file_path)
+            .flatten()
+            .map(|line| {
+                let (hand, bet) = line.split_once(" ").unwrap();
+                let bet: i64 = bet.parse().unwrap();
+                (hand.to_owned(), bet)
+            })
+            .map(|(hand, bet)| (PokerHand::from(hand), bet))
+            .collect();
+        poker_hands.sort_by(|(hand, _), (hand2, _)| hand.cmp(hand2));
+        poker_hands
+            .iter()
+            .enumerate()
+            .map(|(ranking, (_, bet))| (ranking + 1) as i64 * bet)
+            .sum()
     }
 }
